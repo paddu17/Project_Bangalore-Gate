cafc977ac077dcc3777903d5975d0595281754e4
Padmaja Rajagopalan <89189004+paddu17@users.noreply.github.com>
2021-10-07 19:33:36 +0530

    Modified ones

---
+++ footer.php
@@ -0,0 +1,189 @@
+
+
+
+
+
+    <!-- Footer Starts -->
+    <footer>
+        <div class="footer-top">
+            <div class="container">
+                
+                    <div class="row">
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <div class="footer-about">
+                                <h4>Company Info</h4>
+                                <p>“ Hotel Bangalore Gate is a registered and approved 2 star category hotel on Kempegoda road, in Majestic Area of Bengaluru City, India”</p>
+                            </div>
+                            <div class="footer-payment">
+                                <h4>We Accept</h4>
+                                <ul>
+                                    <li><img src="images/icons/visa.png" alt="image"></li>
+                                    <li><img src="images/icons/mastercard.png" alt="image"></li>
+                                    <li><img src="images/icons/americanexpress.png" alt="image"></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="quick-links">
+                                <h4>Quick Links</h4>
+                                <ul>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">Rooms</a></li>
+                                    <li><a href="#">Testimonials</a></li>
+                                    <li><a href="#">Blog</a></li>
+                                    <li><a href="#">Gallery</a></li>
+                                    <li><a href="#">Services</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <div class="Rooms">
+                                <h4>Rooms</h4>
+                                <ul>
+                                    <li><a href="#">Single Rooms</a></li>
+                                    <li><a href="#">Double Rooms</a></li>
+                                    <li><a href="#">Studio Rooms</a></li>
+                                    <li><a href="#">Kingsize Rooms</a></li>
+                                    <li><a href="#">Presidentsuite Rooms</a></li>
+                                    <li><a href="#">Luxury Kings Rooms</a></li>
+                                    <li><a href="#">Connecting Rooms</a></li>
+                                    <li><a href="#">Murphy Rooms</a></li>
+                                </ul>
+                            </div>
+                        </div> -->
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <div class="footer-contact">
+                            <h4>Contact info</h4>
+                                <ul>
+                                    <li>mob: +91-99453-33999  |
+                                    
+                                    +91-80-4110-0777</li>
+                                    <li>Email: bangaloregate@gmail.com</li>
+
+                                    <li>Address: No. 9 & 12, Gajanana Towers I, Kempe Gowda Road,  Opposite Menaka Theatre, Near Mysore Bank Circle, Majestic, Bengalooru  - 560 009, Karnataka, India.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+            <div class="row">
+            <div class="col-md-4 col-sm-12 col-xs-12">
+                <div class="copyright-content text-center">
+                <span> </span>
+                    <p class="white">Copyright &copy; <span><a href="http://webosys.com/">Bangalore Gate. </a></span>. 2020 All right reserved </p>
+                    <ul>
+                        <li><a href=" https://www.facebook.com/pages/Hotel-Bangalore-Gate/200512596644489?nr" class="white"><i class="fab fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="https://twitter.com/bangaloregate" class="white"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="https://instagram.com/bangaloregate" class="white"><i class="fab fa-instagram" aria-hidden="true"></i></a></li>
+                        <!-- <li><a href="#" class="white"><i class="fab fa-google-plus" aria-hidden="true"></i></a></li> -->
+                    </ul>
+                </div>
+                </div>
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                       
+                    </div>
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                        <div class="copyright-links mar-bottom-20">
+                            <ul>
+                                <li><a href="#">Accomodations</a></li>
+                                <li><a href="#">Book Now</a></li>
+                                <li><a href="#">Terms and Conditions</a></li>
+                            </ul>
+                        </div>
+                        <!-- <div class="playstore-links">
+                            <img src="images/icons/appstore.png" alt="image" class="mar-right-10">
+                            <img src="images/icons/googleplay.png" alt="image">
+                        </div> -->
+            </div>
+        </div>
+    </footer>
+    <!-- Footer Ends -->
+    
+    <!-- Back to top start -->
+    <div id="back-to-top" style="display:block;">
+        <a href="#"></a>
+    </div>
+    <!-- Back to top ends -->
+
+    <div class="modal fade" id="login" role="dialog">
+        <div class="modal-dialog">
+            <div class="login-content">
+                <div class="login-image">
+                    <img src="images/logo-black.png" alt="image">
+                </div>
+                <h3>Hello!Sign into your account</h3>
+                <form>
+                    <div class="form-group">
+                        <input type="email" placeholder="Enter email address">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" placeholder="Enter password">
+                    </div>
+                    <div class="form-group form-checkbox">
+                        <input type="checkbox"> Remember Me
+                        <a href="#">Forgot password?</a>
+                    </div>
+                </form>
+                <div class="form-btn">
+                    <a href="#" class="btn btn-orange">LOGIN</a>
+                    <p>Need an Account?<a href="#"> Create your Hotux account</a></p>
+                </div>
+                <ul class="social-links">
+                    <li><a href="#"><i class="fab fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fab fa-linkedin" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="register" role="dialog">
+        <div class="modal-dialog">
+            <div class="login-content">
+                <div class="login-image">
+                    <img src="images/logo-black.png" alt="image">
+                </div>
+                <h3>Awesome! Create a Hotux Account</h3>
+                <form>
+                    <div class="form-group">
+                        <input type="text" placeholder="Enter username">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" placeholder="Enter email address">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" placeholder="Enter password">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" placeholder="Confirm password">
+                    </div>
+                </form>
+                <div class="form-btn">
+                    <a href="#" class="btn btn-orange">SIGN UP</a>
+                </div>
+                <ul class="social-links">
+                    <li><a href="#"><i class="fab fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa-bed fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fab fa-linkedin" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <!-- *Scripts* -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/plugin.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/custom-nav.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,127 @@
+<style type="text/css">
+    
+    .header_menu.affix a p
+    {
+        color: white!important; /* changed from black to white so as to make the text visible*/
+    }
+    .header_menu.affix
+    {
+        position: fixed !important;
+    }
+    .header-content{
+        background: black;
+    }
+    /* Added code to display the header completely*/
+    .container{
+        background: black;
+    }   
+    
+</style>
+
+
+
+    <!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="zxx">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bangalore Gate</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <!--Default CSS-->
+    <link href="css/default1.css" rel="stylesheet" type="text/css">
+    <!--Custom CSS-->
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <!--Plugin CSS-->
+    <link href="css/plugin.css" rel="stylesheet" type="text/css">
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    ​​<!-- Google Tag Manager -->
+​​<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+​​new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+​​j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+​​'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+​​})(window,document,'script','dataLayer','GTM-M5QPCXK');</script>
+​​<!-- End Google Tag Manager -->
+</head>
+<body style="margin-top: -20px;">
+​​<!-- Google Tag Manager (noscript) -->
+​​<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5QPCXK"
+​​height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+​​<!-- End Google Tag Manager (noscript
+    <!-- Preloader -->
+    <div id="preloader">
+        <div id="status"></div>
+    </div>
+    <!-- Preloader Ends -->
+
+    <!-- header start -->
+    <header class="main_header_area">
+        <div class="header-content">
+            <div class="container">
+                <div class="links links-left">
+                    <ul>
+                        <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i> bangaloregate@gmail.com</a></li>
+                        <li><a href="#"><i class="fa fa-mobile" aria-hidden="true"></i> +91-99453-33999</a></li>
+                        <li>
+                            <select>
+                                <option>Eng</option>
+                                <option>Fra</option>
+                                <option>Esp</option>
+                            </select>
+                        </li>
+                    </ul>
+                </div>
+                <div class="links links-right pull-right">
+                    <ul>
+                        <li><a href="#" data-toggle="modal" data-target="#login"><i class="fa fa-user" aria-hidden="true"></i> Login</a></li>
+                        <li><a href="#" data-toggle="modal" data-target="#register"><i class="fa fa-pen" aria-hidden="true"></i> Register</a></li> 
+                        <li>
+                            <ul class="social-links">
+                                <li><a href="#"><i class="fab fa-facebook" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fab fa-google-plus" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- Navigation Bar -->
+        <div class="header_menu affix-top">
+            <nav class="navbar navbar-default">
+                <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="index.php">
+                            <p style="color:#fff;"> Bangalore Gate </p>
+                        </a>
+                    </div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav" id="responsive-menu">
+                        <li><a href="index.php">Home</a></li>
+                         
+                         <li><a href="">location </a></li>
+                           <li><a href="">Rooms</a></li>
+                          
+                                   <li><a href="">Events </a></li>
+                                    
+                           <li><a href="">restaurant</a></li>  
+                        </ul>
+                      
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+                <div id="slicknav-mobile"></div>
+            </nav>
+        </div>
+        <!-- Navigation Bar Ends -->
+    </header>
+    <!-- header Ends -->
+
